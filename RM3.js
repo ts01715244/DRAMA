0cad8c969a91f391c1f2987daf883cc5fef4302b
ts01715244 <ts01715244@gmail.com>
2016-09-28 12:22:47 +0800

    RM
    RM

---
+++ RM3.js
@@ -0,0 +1,8 @@
+function videoGG3(id,name) { 
+  str = '<marquee>'+name+'</marquee><iframe src="https://docs.google.com/file/d/'+id+'/preview" width="100%" height="350" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>';
+  return str;
+}
+
+function videoG3(id,name) {
+  document.getElementById("player1").innerHTML = videoGG3(id,name);		
+}
