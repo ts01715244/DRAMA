bcabac527c59ddd71dbd368269a190ab2c5fac18
ts01715244 <ts01715244@gmail.com>
2016-09-28 12:03:08 +0800

    Update RM.js

--- RM.js
+++ RM.js
@@ -34,11 +34,11 @@ function readGG(id) {
   document.getElementById("player").innerHTML = read(id);		
 }
 
-function videoGG(id,name) { 
+function videoGG1(id,name) { 
   str = '<marquee>'+name+'</marquee><iframe src="https://docs.google.com/file/d/'+id+'/preview" width="100%" height="350" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>';
   return str;
 }
 
 function videoG(id,name) {
-  document.getElementById("player1").innerHTML = videoGG(id,name);		
+  document.getElementById("player1").innerHTML = videoGG1(id,name);		
 }
